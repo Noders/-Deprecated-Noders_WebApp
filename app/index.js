21c3e571a5f08aec6b71f794708619d1181f5f52
fforres mac <felipe.torressepulveda@gmail.com>
2015-06-11 11:55:44 -0300

    adding angular-formly, lumx template for angular formly, migratin normal form tu angular formly form, on the creation of events, missing login and register

--- app/index.js
+++ app/index.js
@@ -1,14 +1,22 @@
 require('./vendor')();
 const angular = require('angular');
 require('./bower_components/lumx/dist/lumx.min.js');
+require('./bower_components/angular-formly-templates-lumx/dist/formlyLumx.js');
+require('./bower_components/angular-formly-templates-lumx/dist/formlyLumx.css');
 
 const ngModule = angular.module('app', [
     require('angular-ui-router'),
     require('angular-resource'),
     require('angular-material'),
+    require('angular-messages'),
+    require('angular-aria'),
+    require('angular-formly'),
     'lbServices',
     'lumx',
+	'formlyLumx',
     'youtube-embed'
 ]);
+
+
 require('./config')(angular, ngModule);
 require('./modules')(angular, ngModule);
--- app/modules/events/controllers/events.controller.js
+++ app/modules/events/controllers/events.controller.js
@@ -32,11 +32,199 @@ export default (ngModule) => {
                             LxProgressService.circular.hide();
                             $scope.eventos = eventos;
                         });
-                    };
+                };
 
                 $scope.auth = LoopBackAuth;
                 $scope.error = {};
+
+
+                $scope.formData = {}; // the data object
+                $scope.formOptions = {}; // optional form parameters
+                $scope.formFields = [{ // an array holding all form fields
+                        key: 'nombre', // ng-model name, saved in formData
+                        type: 'lx-input', // field
+                        templateOptions: { // in this case: 'lx-input' options
+                            type: 'email',
+                            label: 'Nombre del evento',
+                            required: true
+                                //fixedLabel: true,
+                                //icon: 'mail',
+
+                        }
+                    }, {
+                        key: 'url', // ng-model name, saved in formData
+                        type: 'lx-input', // field
+                        templateOptions: { // in this case: 'lx-input' options
+                            type: 'url',
+                            label: 'URL del evento (Publicidad o suscripción)',
+                            required: true
+                                //fixedLabel: true,
+                                //icon:'letter'
+                        }
+                    }, {
+                        key: 'latitud', // ng-model name, saved in formData
+                        type: 'lx-input', // field
+                        templateOptions: { // in this case: 'lx-input' options
+                            type: 'number',
+                            label: 'Latitud',
+                            required: true
+                                //fixedLabel: true,
+                                //icon:'letter'
+                        }
+                    }, {
+                        key: 'longitud', // ng-model name, saved in formData
+                        type: 'lx-input', // field
+                        templateOptions: { // in this case: 'lx-input' options
+                            type: 'number',
+                            label: 'Longitud',
+                            required: true
+                                //fixedLabel: true,
+                                //icon:'letter'
+                        }
+                    }, {
+                        key: 'fechadeinicio',
+                        type: 'lx-date-picker',
+                        templateOptions: {
+                            label: 'Fecha de Inicio',
+                            required: true
+                        }
+                    },
+
+
+                    {
+                        "type": "lx-flex",
+                        "templateOptions": {
+                            "flex": {
+                                "container": "row",
+                                "item": 6
+                            },
+                            "fields": [
+
+                                {
+                                    key: 'horadeinicio',
+                                    type: 'lx-select',
+                                    templateOptions: {
+                                        "placeholder": "Hora",
+                                        "selected": "name",
+                                        "choice": "name",
+                                        "options": [{
+                                            "name": "Adam"
+                                        }, {
+                                            "name": "Amalie"
+                                        }, {
+                                            "name": "Wladimir"
+                                        }, {
+                                            "name": "Samantha"
+                                        }, {
+                                            "name": "Estefania"
+                                        }]
+                                    }
+                                }, {
+                                    key: 'minutodeinicio',
+                                    type: 'lx-select',
+                                    templateOptions: {
+                                        "placeholder": "Choose a Person",
+                                        "selected": "name",
+                                        "choice": "name",
+                                        "options": [{
+                                            "name": "Adam"
+                                        }, {
+                                            "name": "Amalie"
+                                        }, {
+                                            "name": "Wladimir"
+                                        }, {
+                                            "name": "Samantha"
+                                        }, {
+                                            "name": "Estefania"
+                                        }]
+                                    }
+                                }
+                            ]
+                        }
+                    }, {
+                        key: 'fechadetermino',
+                        type: 'lx-date-picker',
+                        templateOptions: {
+                            label: 'Fecha de Inicio',
+                            required: true
+                        }
+                    },
+
+                    {
+                        "type": "lx-flex",
+                        "templateOptions": {
+                            "flex": {
+                                "container": "row",
+                                "item": 6
+                            },
+                            "fields": [
+
+                                {
+                                    key: 'horadetermino',
+                                    type: 'lx-select',
+                                    templateOptions: {
+                                        "placeholder": "Choose a Person",
+                                        "selected": "name",
+                                        "choice": "name",
+                                        "options": [{
+                                            "name": "Adam"
+                                        }, {
+                                            "name": "Amalie"
+                                        }, {
+                                            "name": "Wladimir"
+                                        }, {
+                                            "name": "Samantha"
+                                        }, {
+                                            "name": "Estefania"
+                                        }]
+                                    }
+                                }, {
+                                    key: 'minutodetermino',
+                                    type: 'lx-select',
+                                    templateOptions: {
+                                        "placeholder": "Choose a Person",
+                                        "selected": "name",
+                                        "choice": "name",
+                                        "options": [{
+                                            "name": "Adam"
+                                        }, {
+                                            "name": "Amalie"
+                                        }, {
+                                            "name": "Wladimir"
+                                        }, {
+                                            "name": "Samantha"
+                                        }, {
+                                            "name": "Estefania"
+                                        }]
+                                    }
+                                }
+                            ]
+                        }
+                    }
+
+
+
+
+                    , {
+                        key: 'fechadetermino',
+                        type: 'lx-date-picker',
+                        templateOptions: {
+                            label: 'Fecha de Término',
+                            required: true
+                        }
+                    }, {
+                        key: 'horadetermino',
+                        type: 'lx-date-picker',
+                        templateOptions: {
+                            label: 'Fecha de Inicio',
+                            required: true
+                        }
+                    }
+                ];
+
+
+
+
             }
         ]);
-
 };
